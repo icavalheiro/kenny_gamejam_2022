ea3dee75327b41cb373d70e5fedd64bdfb18e923
Ivan Cavalheiro <ivan.cavalheiro@hogarthww.com>
2022-08-19 18:57:43 -0300

    tryed to import a tilemap from kenney and created a basic hexago tileset implementation

---
+++ Scripts/Tile.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+public class Tile : Spatial
+{
+    public Tile Top;
+    public Tile TopLeft;
+    public Tile TopRight;
+    public Tile Bottom;
+    public Tile BottomLeft;
+    public Tile BottomRight;
+
+    public TilesetModel Model;
+
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    // public override void _Ready()
+    // {
+
+    // }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+
+
+}
